c1e856e2c266366e5a33787d3b7b7a1f583ae6d8
Tamási Benjamin <halftome@yahoo.com>
2016-02-09 23:41:14 +0100

    RequestInterface update and documentation

--- src/RequestInterface.php
+++ src/RequestInterface.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: lejla
- * Date: 2016.02.09.
- * Time: 17:34
- */
 
 namespace CurlX;
 
@@ -12,56 +6,115 @@ namespace CurlX;
  * Interface RequestInterface
  * @package CurlX
  *
- * @property string $url
- * @property array $post
- * @property float $time
- * @property int $timeout
- * @property array $options
- * @property array $headers
- * @property resource $handle
- * @property callable[] $listeners
- * @property mixed $response
+ * @property string $url Url of the Request
+ * @property array $post array of post data
+ * @property float $time running time of the request
+ * @property int $timeout time (in msec) after which the request will be aborted
+ * @property array $options CURL options of the request
+ * @property array $headers headers of the request
+ * @property resource $handle CURL handle of the request
+ * @property callable[] $listeners array of registered listeners which will be called upon when request finishes
+ * @property mixed $response curl's response
  */
 interface RequestInterface
 {
-    function getUrl();
-
-    function setUrl($url);
-
-    function getPostData();
-
-    function setPostData(array $postData);
-
-    function getTime();
-
-    function startTimer();
-
-    function stopTimer();
-
-    function getResult();
+    /**
+     * Getter for url field
+     * @return string url
+     */
+    public function getUrl();
 
     /**
-     * This gets called when a request has completed
-     * @param $result
+     * Setter for the url field
+     * @param string $url url
      */
-    function callBack($result);
+    public function setUrl($url);
+
+    /**
+     * Getter for the post data array
+     * @return array post data
+     */
+    public function getPostData();
+
+    /**
+     * Setter for the post data array
+     * @param array $postData post data
+     */
+    public function setPostData(array $postData);
+
+    /**
+     * Returns the time (msec) it took to make the request
+     * @return float time
+     */
+    public function getTime();
+
+    /**
+     * Start the request's internal timer
+     */
+    public function startTimer();
+
+    /**
+     * Stops the request's internal timer
+     */
+    public function stopTimer();
+
+    /**
+     * Get the result of a query
+     * @return mixed result
+     */
+    public function getResult();
+
+    /**
+     * This gets called by an agent when a request has completed
+     * @param mixed $result result
+     */
+    public function callBack($result);
 
     /**
      * Add a listener that gets notified when the Request has completed
-     * @param callable $function
-     * @return mixed
+     * @param callable $function callback function
      */
-    function addListener(callable $function);
+    public function addListener(callable $function);
 
-    function setTimeout($mSecs);
+    /**
+     * Set a timeout value for the request
+     * @param float $timeout timeout (msec)
+     */
+    public function setTimeout($timeout);
 
-    function getHandle();
+    /**
+     * Get the timeout value registered for the request
+     * @return float timeout
+     */
+    public function getTimeout();
 
-    function setHeaders(array $headers);
+    /**
+     * Get the cUrl handle for the request
+     * @return resource cUrl handle
+     */
+    public function getHandle();
 
-    function getHeaders();
+    /**
+     * Add headers to the request
+     * @param array $headers headers in ['key' => 'value] format
+     */
+    public function setHeaders(array $headers);
 
-    function setOptions(array $options);
+    /**
+     * Get headers set for the request
+     * @return array headers in ['key' => 'value'] format
+     */
+    public function getHeaders();
 
-    function getOptions();
+    /**
+     * Add cUrl options to the request
+     * @param array $options options in ['key' => 'value'] format
+     */
+    public function setOptions(array $options);
+
+    /**
+     * Get cUrl options set for the request
+     * @return array options in ['key' => 'value'] format
+     */
+    public function getOptions();
 }
